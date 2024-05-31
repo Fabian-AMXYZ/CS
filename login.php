fe5d7bb9bc74f5b0cf0faf219277ed35117a8670
Prince-Pipen <152761702+Prince-Pipen@users.noreply.github.com>
2024-05-31 18:46:01 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,19 @@
 <?php
-// Code to authenticate the user
-header("Location: index.html");
+
+// Retrieve the form data
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Authenticate the user
+if (authenticate_user($username, $password)) {
+    header("Location: index.html");
+} else {
+    header("Location: login.html?error=1");
+}
 exit();
+
+function authenticate_user($username, $password) {
+    // Add code to verify user credentials
+    return true; // or false based on authentication result
+}
 ?>
