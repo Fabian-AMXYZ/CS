5a8a3a9d298cecbcaa8dde415bb1b0038f041924
Prince-Pipen <152761702+Prince-Pipen@users.noreply.github.com>
2024-05-31 19:32:02 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,19 +1,10 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
 
-// Retrieve the form data
-$username = $_POST['username'];
-$password = $_POST['password'];
+    // Perform actual login validation
 
-// Authenticate the user
-if (authenticate_user($username, $password)) {
-    header("Location: index.html");
-} else {
-    header("Location: login.html?error=1");
-}
-exit();
-
-function authenticate_user($username, $password) {
-    // Add code to verify user credentials
-    return true; // or false based on authentication result
+    echo "Logged in as $username";
 }
 ?>
