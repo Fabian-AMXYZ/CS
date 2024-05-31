63535c9564d0384117c91a2ed77c0ac87842075e
Prince-Pipen <152761702+Prince-Pipen@users.noreply.github.com>
2024-05-31 18:46:29 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,19 @@
 <?php
-// Code to register the user
-header("Location: index.html");
+
+// Retrieve the form data
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Register the user
+if (register_user($username, $password)) {
+    header("Location: index.html");
+} else {
+    header("Location: register.html?error=1");
+}
 exit();
+
+function register_user($username, $password) {
+    // Add code to register the user
+    return true; // or false based on registration result
+}
 ?>
